646b352de410a0de33445c3dd08e4d2f7a059a6c
Rfed0r0v <rf.rf944@gmail.com>
2022-05-30 19:08:12 +0300

    Metods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private static void printYearType(int year) {
+        if (year % 400 == 0 ||(year % 100!=0 && year % 4 ==0)) {System.out.println (year + " является високосным");}
+        else {System.out.println (year + " не является високосным");}
     }
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        printYearType(2100);
+
+
+
+    }
+
+
 }
